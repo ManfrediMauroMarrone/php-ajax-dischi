07f16de5dec3e9cfdbe9982a3bdefdc3e9db18c5
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-12-17 16:07:03 +0100

    inizializzato laravel mix e inserito header

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+include 'dischi.php';
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="dist/app.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Andika+New+Basic:ital@1&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="header-container">
+          <div class="logo">
+            <img src="icons/spotify.png" alt="">
+          </div>
+
+          <h1>PHP-Dischi</h1>
+
+        </div>
+      </div>
+    </header>
+    <main>
+      <div class="container">
+        <div class="card">
+          <div class="image">
+            <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+
+          </div>
+          <em>title</em>
+          <h5>title</h5>
+          <h5>title</h5>
+          <h5>title</h5>
+
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
